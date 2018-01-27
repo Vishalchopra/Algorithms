27b81b76b0fa194fe8bf831b6e0c01aa79cdd00b
root <root@localhost.localdomain>
2018-01-27 18:03:05 +0530

    complete merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -3,14 +3,20 @@
 #define my_sizeof(type) (char *)(&type + 1) - (char *)(&type)
 
 void merge_sort(int *, int , int);
+void merge_array(int *, int, int, int);
 
 int main()
 {
-	int arr[] = {5, 3, 4,1,9, 33};
-	int len;
+	int arr[] = {5, 3, 4,1,9, 33, 0, 43, 7, 1000};
+	int len, loopVar;
 	len = (my_sizeof(arr))/(my_sizeof(arr[0]));
 
-	merge_sort(arr, arr[0], arr[len - 1]);
+	merge_sort(arr, 0, len - 1);
+
+	for (loopVar = 0; loopVar < len; loopVar++)
+		printf("%d ", arr[loopVar]);
+
+	printf("\n");
 	return 0;
 }
 
@@ -21,10 +27,9 @@ void merge_sort(int *arr, int start, int end)
 		return;
 	int mid;
 	mid = (end + start)/2;
-
 	merge_sort(arr, start, mid);
 	merge_sort(arr, mid + 1, end);
-	merge_array(arr, start, mid, end)
+	merge_array(arr, start, mid, end);
 }
 
 void merge_array(int *arr, int start, int mid, int end)
@@ -33,15 +38,18 @@ void merge_array(int *arr, int start, int mid, int end)
 	len1 = mid - start + 1;
 	len2 = end - mid;
 	int leftArr[len1], rightArr[len2];
-	for (loopVar1 = 0; loopVar1 < len1; loopVar1++)
-		leftArr[loopvar1] = *(arr + start + loopVar1);
-	for (loopVar2 = 0; loopVar2 < len1; loopVar2++)
-		rightArr[loopvar2] = *(arr + mid + loopVar1 + 1);
+	for (loopVar1 = 0; loopVar1 < len1; loopVar1++){
+		leftArr[loopVar1] = *(arr + start + loopVar1);
+	}
+	for (loopVar2 = 0; loopVar2 < len2; loopVar2++){
+		rightArr[loopVar2] = *(arr + mid + loopVar2 + 1);
+	}
 	loopVar2 = loopVar3 = 0;
-	for (loopvar1 = 0; loopVar1 < end; loopVar1++){
-		if ((leftArr[loopVar1] <= rightArr[loopVar1]) && loopVar2 < len1){
+	for (loopVar1 = start; loopVar1 <= end; loopVar1++){
+		if (((leftArr[loopVar2] <= rightArr[loopVar3]) && (loopVar2 < len1)) || (loopVar3 >= len2)){
+
 			*(arr + loopVar1) = leftArr[loopVar2++];
-		} else {
+		} else  {
 			*(arr + loopVar1) = rightArr[loopVar3++];
 		}
 	}
