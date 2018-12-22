91b3def1de5b1af5368225b4aa54ff2af9dbc755
root <root@localhost.localdomain>
2018-12-22 17:54:17 +0530

    binary tree with pre,in,post order recursive and non-recursive

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -3,19 +3,34 @@
 
 struct binary_tree{
 	int data;
-	struct binary_tree *left;
-	struct binary_tree *right;
+	struct binary_tree *left, *right;
+};
+
+struct stackData {
+	struct binary_tree *current;
+	struct stackData *next;
+};
+
+struct Stack {
+	int count;
+	struct stackData *stackAddr;
 };
 
 enum {
 	INSERT=1,
 	DISPLAY,
+	DISPLAY_PREORDER_NONREC,
 };
 
 /**************************Function Prototype**********************/
 void insert_binary_tree(struct binary_tree **, int);
 struct binary_tree *create_new_node(int);
 void display_binary(struct binary_tree *);
+struct Stack *create_stack();
+void push (struct Stack *, struct binary_tree *);
+void traverse_preorder(struct binary_tree *);
+struct binary_tree *pop (struct Stack *);
+int isStackEmpty(struct Stack *);
 
 
 /**************************Main Function**************************/
@@ -25,8 +40,9 @@ int main()
 
 	while (1){
 		int choice = 0;
-		printf("Press 1 for insert an element\n");
+		printf("\nPress 1 for insert an element\n");
 		printf("Press 2 for display binary tree\n");
+		printf("Press 3 for display binary tree in non recursive way\n");
 
 		scanf("%d", &choice);
 		switch(choice){
@@ -42,6 +58,9 @@ int main()
 		
 		case DISPLAY:
 			display_binary(start);
+		break;
+		case DISPLAY_PREORDER_NONREC:
+			traverse_preorder(start);
 		break;
 		default:
 			printf("Enter a valid option\n");
@@ -67,8 +86,6 @@ void insert_binary_tree(struct binary_tree **start, int data)
 			loopVar = loopVar->left;
 		else
 			loopVar = loopVar->right;
-
-		
 	}
 
 	if (temp == NULL){
@@ -77,8 +94,13 @@ void insert_binary_tree(struct binary_tree **start, int data)
 	}
 	if (temp->data > data)
 		temp->left = new_binary_tree;
-	else
+	else if (temp->data < data)
 		temp->right = new_binary_tree;
+	else{
+		free(new_binary_tree);
+		new_binary_tree = NULL;
+	}
+		
 		
 }
 
@@ -100,9 +122,118 @@ void display_binary(struct binary_tree *start)
 {
 	if (start == NULL)
 		return;
-
 	display_binary(start->left);
 	printf("%d ", start->data);
-
 	display_binary(start->right);
+}
+
+/********************************Pre-Order***************************************/
+/*void traverse_preorder(struct binary_tree *root)
+{
+	struct Stack *stack = create_stack();
+
+	while (1){
+		while (root != NULL){
+			printf("%d ", root->data);
+			push(stack, root);
+			root = root->left;
+		}
+		if (!isStackEmpty(stack))
+			break;
+		root = pop(stack);
+		root = root->right;
+	}
+	printf("\n");
+	free(stack);
+	stack = NULL;
+}*/
+/////////second method
+void traverse_preorder(struct binary_tree *root)
+{
+	struct Stack *stack = create_stack();
+	struct stackData *stackList = NULL;
+	push(stack, root);
+	//stackList = stack->stackAddr;
+	while (stack->count){
+		printf("%d ", stack->stackAddr->current->data);
+		pop(stack);
+		if (root->right)
+			push(stack, root->right);
+		if (root->left)
+			push(stack, root->left);
+		if (stack->stackAddr)
+			root = stack->stackAddr->current;
+		//stackList = stack->stackAddr;
+	}
+}
+
+/**********************************Traverse Inorder*************************************/
+void traverse_inorder(struct binary_tree *root)
+{
+	struct Stack *stack = create_stack();
+	while (1){
+		while (root){
+			push (stack, root);
+			root = root->left;
+		}
+		if (!isStackEmpty(stack))
+			break;
+		printf("%d ", stack->stackAddr->current->data);//print top element in stack
+		root = stack->stackAddr->current->right;
+		pop(stack);
+	}
+}
+
+/*******************************Create Stack*********************************************/
+struct Stack *create_stack()
+{
+	struct Stack *S = NULL;
+	S = (struct Stack *)malloc(sizeof(struct Stack *));
+	S->count = 0;
+	S->stackAddr = NULL;
+	return S;	
+		
+}
+
+void push (struct Stack *stack, struct binary_tree *nodeAddr)
+{
+	if (nodeAddr == NULL){
+		printf("Please check the data\n");
+		return;
+	}
+	struct stackData *top;
+	top = (struct stackData *)malloc(sizeof(struct stackData));
+	if (0 == stack->count){
+		stack->count++;
+		top->current = nodeAddr;
+		top->next = NULL;
+	} else {
+		stack->count++;
+		top->current = nodeAddr;
+		top->next = stack->stackAddr;
+		
+	}
+	stack->stackAddr = top;
+}
+
+struct binary_tree *pop (struct Stack *stack)
+{
+	struct binary_tree *temp = NULL;
+	struct stackData *temp2 = NULL;
+	if (stack == NULL && stack->stackAddr == NULL){
+		printf ("Nothing to pop\n");
+		return ;
+	}
+	stack->count--;
+	temp = stack->stackAddr->current;
+	temp2 = stack->stackAddr;
+	stack->stackAddr = stack->stackAddr->next;
+	free(temp2);
+	temp2 = NULL;
+	return temp;
+
+}
+int isStackEmpty(struct Stack *stack)
+{
+	return stack->count;
 }
